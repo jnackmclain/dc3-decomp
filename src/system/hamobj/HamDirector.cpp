78333538f476b78cd4634cdc3ee89ac7fa8c4669
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-09-26 19:13:43 -0700

    hamdirector context

--- src/system/hamobj/HamDirector.cpp
+++ src/system/hamobj/HamDirector.cpp
@@ -5,25 +5,30 @@
 #include "obj/Data.h"
 #include "obj/Object.h"
 #include "os/Debug.h"
+#include "rndobj/Draw.h"
+#include "utl/Symbol.h"
 
 HamDirector *TheHamDirector;
 
 HamDirector::HamDirector()
-    : unk8c(this), unka0(this), unkb4(this), unkc8(0), unkcc(""), unkd0(1), unkd4(this),
-      unke8(this), unkfc(this), unk110(this), unk124(this), unk140(0), unk14c(0),
-      unk150(this), unk164(this), unk178(this), unk18c(this), unk1a0(0), unk1a4(1),
-      unk1a8(this), unk1bc(this), unk1d0(0), unk1d4(0), unk1d8(this), unk1ec(this),
-      unk200(1), unk204(this), unk218(this), unk22c(this), unk240(this), unk254(0),
-      unk259(0), unk25a(0), unk25c(this), unk270(this), unk284(this), unk29c(-kHugeFloat),
-      unk2a0(0), unk2a1(0), unk2a4(0), unk2a8(-kHugeFloat), unk2ac(1), unk2bc(0),
-      unk2bd(0), unk2c0(this), unk2d4(0), unk2d8(0), unk2dc(1), unk2e0(1), unk2e8(0),
-      unk304(0), unk308(this), unk31c(this), unk334(0), unk335(1), unk336(1), unk338(0),
+    : unk8c(this), unka0(this), unkb4(this), unkc8(0), unkcc(""), mBackupDrift(1),
+      mMerger(this), mMoveMerger(this), mGameModeMerger(this), mCurWorld(this),
+      unk124(this), unk140(0), unk14c(0), unk150(this), mCamPostProc(this),
+      mForcePostProc(this), unk18c(this), mForcePostProcBlend(0),
+      mForcePostProcBlendRate(1), unk1a8(this), unk1bc(this), unk1d0(0), unk1d4(0),
+      unk1d8(this), unk1ec(this), mFreestyleEnabled(1), unk204(this), unk218(this),
+      unk22c(this), unk240(this), unk254(0), mDisabled(0), unk25a(0), mCurShot(this),
+      unk270(this), unk284(this), unk29c(-kHugeFloat), mDisablePicking(0), unk2a1(0),
+      unk2a4(0), unk2a8(-kHugeFloat), unk2ac(1), mPlayerFreestyle(0),
+      mPlayerFreestylePaused(0), unk2c0(this), mPracticeStart(0), mPracticeEnd(0),
+      mStartLoopMargin(1), mEndLoopMargin(1), mBlendDebug(0), unk304(0), unk308(this),
+      unk31c(this), mNoTransitions(0), mCollisionChecks(1), mLoadedNewSong(1), unk338(0),
       unk33c(RandomInt(0, 2)), unk33d(0), unk340(this), unk354(this), unk369(0),
       unk36c(0) {
     static DataNode &n = DataVariable("hamdirector");
     n = this;
     TheHamDirector = this;
-    unk2b0.reserve(100);
+    mDirCutKeys.reserve(100);
 }
 
 HamDirector::~HamDirector() {
@@ -35,3 +40,54 @@ HamDirector::~HamDirector() {
         TheHamDirector = nullptr;
     }
 }
+
+BEGIN_PROPSYNCS(HamDirector)
+    SYNC_PROP_SET(shot, mShot, SetShot(_val.Sym()))
+    static Symbol none("none");
+    SYNC_PROP_SET(postproc, NULL_OBJ, )
+    SYNC_PROP_SET(world_event, none, SetWorldEvent(_val.Sym()))
+    SYNC_PROP_SET(clip, ClosestMove(), )
+    SYNC_PROP_SET(practice, Symbol(), )
+    SYNC_PROP_SET(move, Symbol(), )
+    SYNC_PROP_SET(move_instance, Symbol(), )
+    SYNC_PROP_SET(move_parents, Symbol(), )
+    SYNC_PROP_SET(clip_crossover, Symbol(), )
+    SYNC_PROP(merger, mMerger)
+    SYNC_PROP(game_mode_merger, mGameModeMerger)
+    SYNC_PROP(move_merger, mMoveMerger)
+    SYNC_PROP(disable_picking, mDisablePicking)
+    SYNC_PROP_SET(player_freestyle, mPlayerFreestyle, UpdatePlayerFreestyle(_val.Int()))
+    SYNC_PROP_SET(
+        pause_player_freestyle, mPlayerFreestylePaused, PausePlayerFreestyle(_val.Int())
+    )
+    SYNC_PROP(force_postproc, mForcePostProc)
+    SYNC_PROP(force_postproc_blend, mForcePostProcBlend)
+    SYNC_PROP(force_postproc_blend_rate, mForcePostProcBlendRate)
+    SYNC_PROP(disabled, mDisabled)
+    SYNC_PROP(excitement, mExcitement)
+    SYNC_PROP(num_players_failed, mNumPlayersFailed)
+    SYNC_PROP(cam_postproc, mCamPostProc)
+    SYNC_PROP_SET(cur_shot, mCurShot.Ptr(), )
+    SYNC_PROP_SET(cur_world, mCurWorld.Ptr(), )
+    SYNC_PROP_SET(backup_drift, mBackupDrift, )
+    SYNC_PROP_SET(spot_instructor, Symbol("off"), SetCharSpot("instructor", _val.Sym()))
+    SYNC_PROP(practice_start, mPracticeStart)
+    SYNC_PROP(practice_end, mPracticeEnd)
+    SYNC_PROP(start_loop_margin, mStartLoopMargin)
+    SYNC_PROP(end_loop_margin, mEndLoopMargin)
+    SYNC_PROP(blend_debug, mBlendDebug)
+    SYNC_PROP(no_transitions, mNoTransitions)
+    SYNC_PROP(collision_checks, mCollisionChecks)
+    SYNC_PROP_SET(
+        dancer_face_clip, GetMainFaceOverrideClip(), SetMainFaceOverrideClip(_val.Sym())
+    )
+    SYNC_PROP_SET(
+        dancer_face_weight,
+        GetMainFaceOverrideWeight(),
+        SetMainFaceOverrideWeight(_val.Float())
+    )
+    SYNC_PROP(freestyle_enabled, mFreestyleEnabled)
+    SYNC_PROP(loaded_new_song, mLoadedNewSong)
+    SYNC_SUPERCLASS(RndDrawable)
+    SYNC_SUPERCLASS(Hmx::Object)
+END_PROPSYNCS
--- src/system/hamobj/HamDirector.h
+++ src/system/hamobj/HamDirector.h
@@ -19,6 +19,7 @@
 
 class AnimPtr;
 
+/** "Hammer Director, sits in each song file and manages camera + scene changes" */
 class HamDirector : public RndPollable, public RndDrawable {
 public:
     struct DircutEntry {};
@@ -44,8 +45,27 @@ public:
     OBJ_MEM_OVERLOAD(0x6D)
     NEW_OBJ(HamDirector)
 
+    float GetMainFaceOverrideWeight();
+    void SetMainFaceOverrideWeight(float);
+    Symbol GetMainFaceOverrideClip() const;
+    void SetMainFaceOverrideClip(Symbol);
+
 protected:
     HamDirector();
+
+    void SetShot(Symbol);
+
+    /** World event options: (none bonusfx bonusfx_optional chorus verse) */
+    void SetWorldEvent(Symbol);
+
+    Symbol ClosestMove();
+    void UpdatePlayerFreestyle(bool);
+    void SetCharSpot(Symbol, Symbol);
+    void PausePlayerFreestyle(bool pause) {
+        mPlayerFreestylePaused = pause;
+        if (unk2c0)
+            unk2c0->SetShowing(!pause);
+    }
 
     ObjDirPtr<RndDir> unk48; // 0x48
     std::map<Difficulty, AnimPtr> unk5c; // 0x5c
@@ -55,31 +75,36 @@ protected:
     ObjPtr<RndPropAnim> unkb4; // 0xb4
     float unkc8; // 0xc8
     Symbol unkcc; // 0xcc
-    float unkd0; // 0xd0
-    ObjPtr<FileMerger> unkd4; // 0xd4
-    ObjPtr<FileMerger> unke8; // 0xe8
-    ObjPtr<FileMerger> unkfc; // 0xfc
-    ObjPtr<WorldDir> unk110; // 0x110
+    /** "How much backup dancers drift, 0 is none, 1 is full" */
+    float mBackupDrift; // 0xd0
+    ObjPtr<FileMerger> mMerger; // 0xd4
+    ObjPtr<FileMerger> mMoveMerger; // 0xe8
+    ObjPtr<FileMerger> mGameModeMerger; // 0xfc
+    ObjPtr<WorldDir> mCurWorld; // 0x110
     ObjPtr<SongCollision> unk124; // 0x124
     Symbol unk138; // 0x138
     Symbol unk13c; // 0x13c
     bool unk140; // 0x140
-    int unk144; // 0x144
-    int unk148; // 0x148
+    /** "how many have failed" */
+    int mNumPlayersFailed; // 0x144
+    /** "excitement level" */
+    int mExcitement; // 0x148
     bool unk14c; // 0x14c
     ObjPtr<RndPostProc> unk150; // 0x150
-    ObjPtr<RndPostProc> unk164; // 0x164
-    ObjPtr<RndPostProc> unk178; // 0x178
+    /** "camera postproc override.  If set, does no postproc blends" */
+    ObjPtr<RndPostProc> mCamPostProc; // 0x164
+    ObjPtr<RndPostProc> mForcePostProc; // 0x178
     ObjPtr<RndPostProc> unk18c; // 0x18c
-    float unk1a0; // 0x1a0
-    float unk1a4; // 0x1a4
+    float mForcePostProcBlend; // 0x1a0
+    float mForcePostProcBlendRate; // 0x1a4
     ObjPtr<RndPostProc> unk1a8; // 0x1a8
     ObjPtr<RndPostProc> unk1bc; // 0x1bc
     float unk1d0; // 0x1d0
     float unk1d4; // 0x1d4
     ObjPtr<RndPostProc> unk1d8; // 0x1d8
     ObjPtr<RndPostProc> unk1ec; // 0x1ec
-    bool unk200; // 0x200
+    /** "TRUE if freestyle is allowed" */
+    bool mFreestyleEnabled; // 0x200
     ObjPtr<HamCharacter> unk204; // 0x204
     ObjPtr<HamCharacter> unk218; // 0x218
     ObjPtr<HamCharacter> unk22c; // 0x22c
@@ -89,28 +114,37 @@ protected:
     bool unk256;
     bool unk257;
     bool unk258;
-    bool unk259;
+    bool mDisabled; // 0x259
     bool unk25a;
-    ObjPtr<HamCamShot> unk25c; // 0x25c
+    /** "currently shown camshot, nice for debugging." */
+    ObjPtr<HamCamShot> mCurShot; // 0x25c
     ObjPtr<HamCamShot> unk270; // 0x270
     ObjPtr<HamCamShot> unk284; // 0x284
-    Symbol unk298; // 0x298
+    /** "HamCamShot category" */
+    Symbol mShot; // 0x298
     float unk29c; // 0x29c
-    bool unk2a0; // 0x2a0
+    bool mDisablePicking; // 0x2a0
     bool unk2a1; // 0x2a1
     int unk2a4; // 0x2a4
     float unk2a8; // 0x2a8
     bool unk2ac; // 0x2ac
-    Keys<DircutEntry, DircutEntry> unk2b0; // 0x2b0
-    bool unk2bc; // 0x2bc
-    bool unk2bd; // 0x2bd
+    Keys<DircutEntry, DircutEntry> mDirCutKeys; // 0x2b0
+    bool mPlayerFreestyle; // 0x2bc
+    bool mPlayerFreestylePaused; // 0x2bd
     ObjPtr<HamVisDir> unk2c0; // 0x2c0
-    Symbol unk2d4; // 0x2d4
-    Symbol unk2d8; // 0x2d8
-    int unk2dc; // 0x2dc
-    int unk2e0; // 0x2e0
+    /** "start frame of practice mode" */
+    Symbol mPracticeStart; // 0x2d4
+    /** "end frame of practice mode" */
+    Symbol mPracticeEnd; // 0x2d8
+    /** "In practice mode, measures before practice_start until loop".
+        Ranges from 1 to 100. */
+    int mStartLoopMargin; // 0x2dc
+    /** "In practice mode, measures after practice_end until loop".
+        Ranges from 1 to 100. */
+    int mEndLoopMargin; // 0x2e0
     int unk2e4; // 0x2e4
-    int unk2e8; // 0x2e8
+    /** "If > 0, is which clip to show by itself rather than doing full blending" */
+    int mBlendDebug; // 0x2e8
     int unk2ec; // 0x2ec
     Symbol unk2f0; // 0x2f0
     Symbol unk2f4[2]; // 0x2f4
@@ -119,9 +153,11 @@ protected:
     ObjPtr<ObjectDir> unk308; // 0x308
     ObjPtr<ObjectDir> unk31c; // 0x31c
     Symbol unk330; // 0x330
-    bool unk334; // 0x334
-    bool unk335; // 0x335
-    bool unk336; // 0x336
+    /** "If true, does not play transitions" */
+    bool mNoTransitions; // 0x334
+    /** "If true, check character collisions when picking cam shots" */
+    bool mCollisionChecks; // 0x335
+    bool mLoadedNewSong; // 0x336
     int unk338; // 0x338
     bool unk33c; // 0x33c
     bool unk33d; // 0x33d
