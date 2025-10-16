946d6e27328d7d6a9a9982138f2d96d260b2e601
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-10-15 18:12:24 -0700

    multimeshproxy

--- src/system/rndobj/MultiMeshProxy.cpp
+++ src/system/rndobj/MultiMeshProxy.cpp
@@ -1,3 +1,51 @@
 #include "rndobj/MultiMeshProxy.h"
+#include "obj/Object.h"
+#include "rndobj/Mesh.h"
 
 RndMultiMeshProxy::RndMultiMeshProxy() : mMultiMesh(this), mIndex(0) {}
+
+BEGIN_HANDLERS(RndMultiMeshProxy)
+END_HANDLERS
+
+BEGIN_PROPSYNCS(RndMultiMeshProxy)
+END_PROPSYNCS
+
+BEGIN_SAVES(RndMultiMeshProxy)
+    MILO_FAIL("Attempting to save a MultiMesh proxy");
+END_SAVES
+
+BEGIN_COPYS(RndMultiMeshProxy)
+    MILO_FAIL("Attempting to copy a MultiMesh proxy");
+END_COPYS
+
+BEGIN_LOADS(RndMultiMeshProxy)
+    MILO_FAIL("Attempting to load a MultiMesh proxy");
+END_LOADS
+
+void RndMultiMeshProxy::DrawShowing() {
+    if (mMultiMesh) {
+        RndMesh *mesh = mMultiMesh->Mesh();
+        if (mesh) {
+            mesh->SetWorldXfm(mIndex->mXfm);
+            mMultiMesh->Mesh()->DrawShowing();
+        }
+    }
+}
+
+void RndMultiMeshProxy::UpdatedWorldXfm() {
+    if (mMultiMesh) {
+        RndMultiMesh::Instance &inst = *mIndex;
+        inst.mXfm = WorldXfm();
+    }
+}
+
+void RndMultiMeshProxy::SetMultiMesh(
+    RndMultiMesh *mesh, const std::list<RndMultiMesh::Instance>::iterator &it
+) {
+    mMultiMesh = nullptr;
+    if (mesh) {
+        SetLocalXfm(it->mXfm);
+    }
+    mMultiMesh = mesh;
+    mIndex = it;
+}
