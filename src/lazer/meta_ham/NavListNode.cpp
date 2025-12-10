d31451eda22bdb09a07db8a94651576126bfc1b2
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-12-09 22:45:35 -0700

    more navlistnodes

--- src/lazer/meta_ham/NavListNode.cpp
+++ src/lazer/meta_ham/NavListNode.cpp
@@ -1,10 +1,16 @@
 #include "meta_ham/NavListNode.h"
+#include "game/GameMode.h"
 #include "obj/Data.h"
 #include "obj/Object.h"
+#include "rndobj/Mat.h"
 #include "ui/UILabel.h"
 #include "ui/UIListCustom.h"
 #include "ui/UIListLabel.h"
+#include "ui/UIListMesh.h"
 #include "utl/Symbol.h"
+#include <cstdio>
+
+#pragma region NavListNode
 
 NavListNode::NavListNode(NavListItemSortCmp *cmp) : mCmp(cmp), mParent(0) {}
 NavListNode::~NavListNode() { RELEASE(mCmp); }
@@ -23,6 +29,9 @@ void NavListNode::GetID(DataArray *a) {
     DataNode &n = a->Node(size);
     n = GetToken();
 }
+
+#pragma endregion
+#pragma region NavListSortNode
 
 NavListSortNode::NavListSortNode(NavListItemSortCmp *cmp)
     : NavListNode(cmp), mShortcut(0), mStartIx(-1) {}
@@ -76,6 +85,9 @@ void NavListSortNode::FinishBuildList(NavListSort *sort) {
 
 void NavListSortNode::SetShortcut(NavListShortcutNode *shortcut) { mShortcut = shortcut; }
 
+#pragma endregion
+#pragma region NavListShortcutNode
+
 NavListShortcutNode::NavListShortcutNode(
     NavListItemSortCmp *cmp, Symbol token, bool localize
 )
@@ -83,10 +95,141 @@ NavListShortcutNode::NavListShortcutNode(
     mParent = nullptr;
 }
 
+NavListShortcutNode::~NavListShortcutNode() { delete mDateTime; }
+
 void NavListShortcutNode::DeleteAll() {
     FOREACH (it, mChildren) {
         (*it)->DeleteAll();
         RELEASE(*it);
     }
     mChildren.clear();
+}
+
+void NavListShortcutNode::FinishSort(NavListSort *sort) {
+    FOREACH (it, mChildren) {
+        (*it)->FinishSort(sort);
+    }
+}
+
+void NavListShortcutNode::FinishBuildList(NavListSort *sort) {
+    FOREACH (it, mChildren) {
+        (*it)->FinishBuildList(sort);
+    }
+}
+
+void NavListShortcutNode::Renumber(std::vector<NavListSortNode *> &nodes) {
+    FOREACH (it, mChildren) {
+        (*it)->Renumber(nodes);
+    }
+}
+
+bool NavListShortcutNode::IsActive() const {
+    FOREACH (it, mChildren) {
+        if ((*it)->IsActive())
+            return true;
+    }
+    return false;
+}
+
+NavListSortNode *NavListShortcutNode::GetFirstActive() {
+    FOREACH (it, mChildren) {
+        NavListSortNode *node = (*it)->GetFirstActive();
+        if (node)
+            return node;
+    }
+    MILO_FAIL("No available result was found for this shortcut.\n");
+    return nullptr;
+}
+
+#pragma endregion
+#pragma region NavListItemNode
+
+NavListSortNode *NavListItemNode::GetFirstActive() {
+    return IsEnabled() ? this : nullptr;
+}
+
+BEGIN_HANDLERS(NavListItemNode)
+    HANDLE_SUPERCLASS(NavListSortNode)
+END_HANDLERS
+
+void NavListItemNode::Text(UIListLabel *listLabel, UILabel *label) const {
+    label->SetTextToken(gNullStr);
+}
+
+RndMat *NavListItemNode::Mat(UIListMesh *mesh) const {
+    if (mesh->Matches("header_back") && !mHeader.Null()) {
+        return mesh->DefaultMat();
+    } else
+        return nullptr;
+}
+
+void NavListItemNode::Renumber(std::vector<NavListSortNode *> &nodes) {
+    mStartIx = nodes.size();
+    nodes.push_back(this);
+}
+
+void NavListItemNode::FinishBuildList(NavListSort *) { mHeader = gNullStr; }
+
+bool NavListItemNode::IsHeader() const { return !mHeader.Null(); }
+Symbol NavListItemNode::HeaderText() const { return mHeader; }
+
+bool NavListItemNode::UseQuickplayPerformer() {
+    static Symbol song_select_quickplay("song_select_quickplay");
+    static Symbol song_select_story("song_select_story");
+    static Symbol song_select_practice("song_select_practice");
+    static Symbol song_select_jukebox("song_select_jukebox");
+    Symbol prop = TheGameMode->Property("song_select_mode")->Sym();
+    return prop == song_select_quickplay || prop == song_select_story
+        || prop == song_select_practice || prop == song_select_jukebox;
+}
+
+#pragma endregion
+#pragma region NavListFunctionNode
+
+BEGIN_HANDLERS(NavListFunctionNode)
+    HANDLE_SUPERCLASS(NavListSortNode)
+END_HANDLERS
+
+Symbol NavListFunctionNode::Select() { return 0; }
+
+void NavListFunctionNode::Renumber(std::vector<NavListSortNode *> &nodes) {
+    mStartIx = nodes.size();
+    nodes.push_back(this);
+}
+
+#pragma endregion
+#pragma region NavListHeaderNode
+
+NavListHeaderNode::NavListHeaderNode(NavListItemSortCmp *cmp, Symbol token, bool localize)
+    : NavListSortNode(cmp), unk44(false), mToken(token), mLocalizeToken(localize),
+      mDateTime(nullptr), mCollapseIconLabel(nullptr) {}
+
+NavListHeaderNode::~NavListHeaderNode() { delete mDateTime; }
+
+BEGIN_HANDLERS(NavListHeaderNode)
+    HANDLE_SUPERCLASS(NavListSortNode)
+END_HANDLERS
+
+Symbol NavListHeaderNode::Select() { return SelectChildren(mChildren, 0); }
+
+bool NavListHeaderNode::IsEnabled() const {
+    FOREACH (it, mChildren) {
+        if ((*it)->IsEnabled())
+            return true;
+    }
+    return false;
+}
+
+void NavListHeaderNode::SetItemCountString(UILabel *label) const {
+    char buffer[8] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };
+    sprintf(buffer, "(%d)", 0);
+    Symbol sym(buffer);
+    label->SetPrelocalizedString(String(sym));
+}
+
+void NavListHeaderNode::SetCollapseStateIcon(bool) const {
+    UILabel *label = GetCollapseIconLabel();
+    if (label) {
+        label->SetTextToken(gNullStr);
+    }
 }
--- src/lazer/meta_ham/NavListNode.h
+++ src/lazer/meta_ham/NavListNode.h
@@ -10,12 +10,12 @@ enum NavListNodeType {
     // stole these from RB3 lmao
     kNodeNone = 0,
     kNodeShortcut = 1,
-    // kNodeHeader = 2,
-    // kNodeSubheader = 3,
-    // kNodeSong = 4,
-    // kNodeFunction = 5,
-    // kNodeSetlist = 6,
-    // kNodeStoreSong = 7
+    kNodeHeader = 2,
+    kNodeSubheader = 3,
+    kNodeItem = 4,
+    kNodeFunction = 5,
+    kNodeSetlist = 6,
+    kNodeStoreSong = 7
 };
 
 class NavListItemSortCmp {
@@ -43,8 +43,8 @@ class NavListNode : public Hmx::Object {
 public:
     NavListNode(NavListItemSortCmp *);
     virtual ~NavListNode();
-    virtual NavListNodeType GetType() const = 0;
-    virtual Symbol GetToken() const = 0;
+    virtual NavListNodeType GetType() const = 0; // 0x58
+    virtual Symbol GetToken() const = 0; // 0x5c
     virtual bool LocalizeToken() const { return true; }
     virtual const DateTime *GetDateTime() const { return nullptr; }
 
@@ -72,14 +72,14 @@ public:
     virtual void OnUnHighlight() = 0;
     virtual void SetCollapseIconLabel(UILabel *) = 0;
     virtual int GetItemCount() = 0;
-    virtual NavListSortNode *GetFirstActive() = 0;
+    virtual NavListSortNode *GetFirstActive() = 0; // 0x88
     virtual void OnContentMounted(const char *, const char *) {}
     virtual void Text(UIListLabel *, UILabel *) const;
     virtual void Custom(UIListCustom *, Hmx::Object *) const;
     virtual RndMat *Mat(UIListMesh *) const { return nullptr; }
-    virtual bool IsActive() const = 0;
-    virtual bool IsEnabled() const = 0;
-    virtual const char *GetAlbumArtPath() = 0;
+    virtual bool IsEnabled() const = 0; // 0x9c
+    virtual bool IsActive() const = 0; // 0xa0
+    virtual const char *GetAlbumArtPath() = 0; // 0xa4
     virtual void DeleteAll();
     virtual void Renumber(std::vector<NavListSortNode *> &);
     virtual void FinishBuildList(NavListSort *);
@@ -102,6 +102,15 @@ public:
     virtual const DateTime *GetDateTime() const { return mDateTime; }
     virtual void DeleteAll();
 
+    void Insert(class NavListItemNode *, class NavListSort *);
+    void
+    InsertHeaderRange(class NavListItemNode **, class NavListItemNode **, class NavListSort *);
+    NavListSortNode *GetFirstActive();
+    bool IsActive() const;
+    void FinishBuildList(NavListSort *);
+    void FinishSort(NavListSort *);
+    void Renumber(std::vector<NavListSortNode *> &);
+
 protected:
     Symbol mToken; // 0x34
     bool mLocalizeToken; // 0x38
@@ -109,6 +118,94 @@ protected:
     std::list<NavListSortNode *> mChildren; // 0x40
 };
 
-// NavListItemNode
-// NavListHeaderNode
-// NavListFunctionNode
+class NavListItemNode : public NavListSortNode {
+public:
+    NavListItemNode(NavListItemSortCmp *cmp) : NavListSortNode(cmp) {}
+    virtual ~NavListItemNode() {}
+    virtual DataNode Handle(DataArray *, bool);
+    virtual NavListNodeType GetType() const { return kNodeItem; }
+    virtual Symbol GetToken() const { return gNullStr; }
+
+    virtual void FinishSort(NavListSort *) {}
+    virtual Symbol OnSelect() { return gNullStr; }
+    virtual Symbol Select() { return gNullStr; }
+    virtual Symbol OnSelectDone() { return gNullStr; }
+    virtual void OnHighlight() {}
+    virtual void OnUnHighlight() {}
+    virtual void SetCollapseIconLabel(UILabel *) {}
+    virtual int GetItemCount() { return 1; }
+    virtual NavListSortNode *GetFirstActive();
+    virtual void OnContentMounted(const char *, const char *) {}
+    virtual void Text(UIListLabel *, UILabel *) const;
+    virtual void Custom(UIListCustom *, Hmx::Object *) const {}
+    virtual RndMat *Mat(UIListMesh *) const;
+    virtual bool IsEnabled() const { return IsEnabled(); } // lmao what
+    virtual bool IsActive() const { return true; }
+    virtual const char *GetAlbumArtPath() { return nullptr; }
+    virtual void Renumber(std::vector<NavListSortNode *> &);
+    virtual void FinishBuildList(NavListSort *);
+
+    bool IsHeader() const;
+    Symbol HeaderText() const;
+    bool UseQuickplayPerformer();
+
+protected:
+    Symbol mHeader; // 0x44
+};
+
+class NavListFunctionNode : public NavListSortNode {
+public:
+    NavListFunctionNode(NavListItemSortCmp *, Symbol, const char *);
+    virtual ~NavListFunctionNode() {}
+    virtual DataNode Handle(DataArray *, bool);
+    virtual NavListNodeType GetType() const { return kNodeFunction; }
+    virtual Symbol GetToken() const { return unk4c; }
+    virtual Symbol Select();
+    virtual bool IsEnabled() const { return IsEnabled(); } // lmao what
+    virtual bool IsActive() const { return false; }
+    virtual const char *GetAlbumArtPath() { return unk44.c_str(); }
+    virtual void Renumber(std::vector<NavListSortNode *> &);
+    virtual int GetTier() const { return -1; }
+    virtual const char *GetShortcutStr() { return gNullStr; }
+    virtual const char *GetArtist() const { return gNullStr; }
+
+protected:
+    String unk44;
+    Symbol unk4c;
+};
+
+class NavListHeaderNode : public NavListSortNode {
+public:
+    NavListHeaderNode(NavListItemSortCmp *, Symbol, bool);
+    virtual ~NavListHeaderNode();
+    virtual DataNode Handle(DataArray *, bool);
+    virtual NavListNodeType GetType() const { return kNodeHeader; }
+    virtual Symbol GetToken() const { return mToken; }
+    virtual bool LocalizeToken() const { return mLocalizeToken; }
+    virtual const DateTime *GetDateTime() const { return mDateTime; }
+    virtual void FinishSort(NavListSort *sort) { NavListSortNode::FinishSort(sort); }
+    virtual Symbol OnSelect() { return gNullStr; }
+    virtual Symbol Select();
+    virtual Symbol OnSelectDone() { return gNullStr; }
+    virtual void OnHighlight() {}
+    virtual void OnUnHighlight() {}
+    virtual void SetCollapseIconLabel(UILabel *label) { mCollapseIconLabel = label; }
+    virtual int GetItemCount() { return 0; }
+    virtual NavListSortNode *GetFirstActive() { return nullptr; }
+    virtual bool IsEnabled() const;
+    virtual bool IsActive() const { return IsActive(); } // ok then
+    virtual const char *GetAlbumArtPath() { return nullptr; }
+    virtual void Insert(NavListItemNode *, NavListSort *);
+    virtual void UpdateItemCount(NavListItemNode *) {}
+    virtual UILabel *GetCollapseIconLabel() const { return mCollapseIconLabel; }
+    virtual Symbol SelectChildren(std::list<NavListSortNode *> &, int);
+    virtual void SetItemCountString(UILabel *) const;
+    virtual void SetCollapseStateIcon(bool) const;
+
+protected:
+    bool unk44;
+    Symbol mToken; // 0x48
+    bool mLocalizeToken; // 0x4c
+    DateTime *mDateTime; // 0x50
+    UILabel *mCollapseIconLabel; // 0x54
+};
