a6124ae4e69d6540690e32ea309cc62e80dcf71f
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-12-14 21:47:00 -0700

    accomplishment

--- src/lazer/meta_ham/Accomplishment.cpp
+++ src/lazer/meta_ham/Accomplishment.cpp
@@ -1,20 +1,18 @@
 #include "lazer/meta_ham/Accomplishment.h"
 #include "obj/Data.h"
-#include "obj/Object.h"
 #include "os/Debug.h"
-#include "ui/UILabel.h"
 #include "utl/MakeString.h"
 #include "utl/Symbol.h"
 
-Accomplishment::Accomplishment(DataArray *a, int i)
-    : mAccomplishment_Name(""), mAccomplishment_Type(0), mCategory(""), mAward(""),
-      mUnits_Token(gNullStr), mDifficulty(0), mPassive_Msg_Channel(gNullStr),
-      mPassive_Msg_Priority(-1), mRequires_Unison_Ability(false), mPlayerCount_Min(-1),
-      mPlayerCount_Max(-1), mNumSongs(-1), mProgressStep(0), mGamerpic_Reward(-1),
-      mAvatarAsset_Reward(-1), mShowBestAfterEarn(true), mHideProgress(false), unk5c(i),
-      mLastID(-1), mEarnedNoFail(true), mLeaderboard(false), isSecondaryGoal(false),
+Accomplishment::Accomplishment(DataArray *cfg, int idx)
+    : mName(""), mAccomplishmentType(), mCategory(""), mAward(""), mUnitsToken(gNullStr),
+      mDifficulty(kDifficultyEasy), mPassiveMsgChannel(gNullStr), mPassiveMsgPriority(-1),
+      mRequiresUnisonAbility(false), mPlayerCountMin(-1), mPlayerCountMax(-1),
+      mNumSongs(-1), mProgressStep(0), mGamerpicReward(-1), mAvatarAssetReward(-1),
+      mShowBestAfterEarn(true), mHideProgress(false), mIndex(idx), mContextID(-1),
+      mEarnedNoFail(true), mLeaderboard(false), mIsSecondaryGoal(false),
       mGiveToAll(false) {
-    Configure(a);
+    Configure(cfg);
 }
 
 Accomplishment::~Accomplishment() {}
@@ -25,173 +23,148 @@ bool Accomplishment::CanBeLaunched() const {
     static Symbol acc_calibrate("acc_calibrate");
     static Symbol acc_charactercreate("acc_charactercreate");
 
-    if (mAccomplishment_Name == acc_calibrate) {
+    if (mName == acc_calibrate) {
         return true;
     } else
-        return mAccomplishment_Name == acc_charactercreate;
+        return mName == acc_charactercreate;
 }
 
-void Accomplishment::UpdateIncrementalEntryName(UILabel *, Symbol) {
-    MILO_ASSERT(false, 0x4c);
+const std::vector<Symbol> &Accomplishment::GetDynamicPrereqsSongs() const {
+    return mDynamicPrereqsSongs;
 }
 
+Symbol Accomplishment::GetName() const { return mName; }
 Symbol Accomplishment::GetCategory() const { return mCategory; }
-
-bool Accomplishment::HasGamerpicReward() const { return mGamerpic_Reward != -1; }
-
-bool Accomplishment::HasAvatarAssetReward() const { return mAvatarAsset_Reward != -1; }
-
+bool Accomplishment::HasGamerpicReward() const { return mGamerpicReward != -1; }
+bool Accomplishment::HasAvatarAssetReward() const { return mAvatarAssetReward != -1; }
 Symbol Accomplishment::GetAward() const { return mAward; }
-
-bool Accomplishment::HasAward() const { return !(mAward == ""); }
-
-bool Accomplishment::IsSecondaryGoal() const { return isSecondaryGoal; }
-
-bool Accomplishment::IsDynamic() const { return !m_vDynamic_Prereq_Songs.empty(); }
+bool Accomplishment::HasAward() const { return mAward != ""; }
+bool Accomplishment::IsSecondaryGoal() const { return mIsSecondaryGoal; }
+bool Accomplishment::IsDynamic() const { return !mDynamicPrereqsSongs.empty(); }
+int Accomplishment::GetDynamicPrereqsNumSongs() const { return mNumSongs; }
+int Accomplishment::GetAvatarAssetReward() const { return mAvatarAssetReward; }
+int Accomplishment::GetContextID() const { return mContextID; }
+int Accomplishment::GetGamerpicReward() const { return mGamerpicReward; }
 
 char const *Accomplishment::GetIconArt() const {
-    return MakeString(
-        "ui/accomplishments/accomplishment_art/%s_keep.png", mAccomplishment_Name.Str()
-    );
+    return MakeString("ui/accomplishments/accomplishment_art/%s_keep.png", mName.Str());
 }
 
 void Accomplishment::Configure(DataArray *i_pConfig) {
     MILO_ASSERT(i_pConfig, 0x34);
-    mAccomplishment_Name = i_pConfig->Sym(0);
+    mName = i_pConfig->Sym(0);
 
-    int launchabledifficulty = 0;
     static Symbol launchable_difficulty("launchable_difficulty");
+    int launchabledifficulty = 0;
     if (i_pConfig->FindData(launchable_difficulty, launchabledifficulty, false)) {
-        mDifficulty = launchabledifficulty;
+        mDifficulty = (Difficulty)launchabledifficulty;
     }
-
     static Symbol launchable_playercount_min("launchable_playercount_min");
-    i_pConfig->FindData(launchable_playercount_min, mPlayerCount_Min, false);
-
+    i_pConfig->FindData(launchable_playercount_min, mPlayerCountMin, false);
     static Symbol launchable_playercount_max("launchable_playercount_max");
-    i_pConfig->FindData(launchable_playercount_max, mPlayerCount_Max, false);
-
+    i_pConfig->FindData(launchable_playercount_max, mPlayerCountMax, false);
     static Symbol launchable_requires_unison_ability("launchable_requires_unison_ability");
-    i_pConfig->FindData(
-        launchable_requires_unison_ability, mRequires_Unison_Ability, false
-    );
-
+    i_pConfig->FindData(launchable_requires_unison_ability, mRequiresUnisonAbility, false);
     static Symbol secret_prereqs("secret_prereqs");
     DataArray *preReqs = i_pConfig->FindArray(secret_prereqs, false);
     if (preReqs) {
         for (int i = 1; i < preReqs->Size(); i++) {
             Symbol s = preReqs->Sym(i);
-            m_vSecret_Prereqs.push_back(s);
+            mSecretPrereqs.push_back(s);
         }
     }
-
     static Symbol dynamic_prereqs("dynamic_prereqs");
     DataArray *dynPreReqs = i_pConfig->FindArray(dynamic_prereqs, false);
     if (dynPreReqs) {
         static Symbol num_songs("num_songs");
         dynPreReqs->FindData(num_songs, mNumSongs, false);
-
         static Symbol songs("songs");
         DataArray *songArray = dynPreReqs->FindArray(songs);
         if (songArray) {
-            for (int i = 0; i < songArray->Size(); i++) {
+            for (int i = 1; i < songArray->Size(); i++) {
                 Symbol s = songArray->Sym(i);
-                m_vDynamic_Prereq_Songs.push_back(s);
+                mDynamicPrereqsSongs.push_back(s);
             }
         }
-        if (m_vDynamic_Prereq_Songs.size() < mNumSongs) {
+        if (mDynamicPrereqsSongs.size() < mNumSongs) {
             MILO_NOTIFY(
                 "There are less songs in the dynamic prereq song list than the num_songs provided: %s",
-                mAccomplishment_Name.Str()
+                mName.Str()
             );
+            mNumSongs = -1;
         }
     }
-
     static Symbol passive_msg_channel("passive_msg_channel");
-    i_pConfig->FindData(passive_msg_channel, mPassive_Msg_Channel, false);
-
+    i_pConfig->FindData(passive_msg_channel, mPassiveMsgChannel, false);
     static Symbol passive_msg_priority("passive_msg_priority");
-    i_pConfig->FindData(passive_msg_priority, mPassive_Msg_Priority, false);
+    i_pConfig->FindData(passive_msg_priority, mPassiveMsgPriority, false);
 
-    if (mPassive_Msg_Channel != gNullStr) {
-        if (mPassive_Msg_Priority < 1) {
+    if (mPassiveMsgChannel != gNullStr) {
+        static int sPriorities[2] = { 1, 1000 };
+        if (mPassiveMsgPriority < 1) {
             MILO_NOTIFY(
                 "Passive Message Priority for goal %s is less than the minimum: %i!",
-                mAccomplishment_Name.Str(),
-                1
+                mName.Str(),
+                sPriorities[0]
             );
-            mPassive_Msg_Priority = 1;
-        } else if (1000 < mPassive_Msg_Priority) {
+            mPassiveMsgPriority = 1;
+        } else if (1000 < mPassiveMsgPriority) {
             MILO_NOTIFY(
                 "Passive Message Priority for goal %s is more than the minimum: %i!",
-                mAccomplishment_Name.Str(),
-                1000
+                mName.Str(),
+                sPriorities[1]
             );
-            mPassive_Msg_Priority = 1000;
+            mPassiveMsgPriority = 1000;
         }
     }
-
     static Symbol progress_step("progress_step");
     i_pConfig->FindData(progress_step, mProgressStep, false);
-
     static Symbol show_best_after_earn("show_best_after_earn");
     i_pConfig->FindData(show_best_after_earn, mShowBestAfterEarn, false);
-
     static Symbol hide_progress("hide_progress");
     i_pConfig->FindData(hide_progress, mHideProgress, false);
-
     static Symbol can_be_earned_with_no_fail("can_be_earned_with_no_fail");
     i_pConfig->FindData(can_be_earned_with_no_fail, mEarnedNoFail, false);
-
     static Symbol leaderboard("leaderboard");
     i_pConfig->FindData(leaderboard, mLeaderboard, false);
-
     static Symbol give_to_all("give_to_all");
     i_pConfig->FindData(give_to_all, mGiveToAll, false);
-
     static Symbol xlast_id("xlast_id");
-    i_pConfig->FindData(xlast_id, mLastID, false);
-
+    i_pConfig->FindData(xlast_id, mContextID, false);
     static Symbol gamerpic_reward("gamerpic_reward");
     DataArray *gamerPicArray = i_pConfig->FindArray(gamerpic_reward, false);
     if (gamerPicArray) {
-        Symbol s = gamerPicArray->Sym(1);
-        if (s == nullptr) {
-            mGamerpic_Reward = gamerPicArray->Int(0);
+        DataNode &n = gamerPicArray->Node(1);
+        if (n.Type() == kDataInt) {
+            mGamerpicReward = n.Int();
         } else {
             MILO_NOTIFY(
-                "Invalid gamerpic_reward for %s (data type 0x%x)", mAccomplishment_Name, s
+                "Invalid gamerpic_reward for %s (data type 0x%x)", mName, n.Type()
             );
         }
     }
-
     static Symbol avatarasset_reward("avatarasset_reward");
     DataArray *avatarArray = i_pConfig->FindArray(avatarasset_reward, false);
     if (avatarArray) {
-        Symbol s = avatarArray->Sym(1);
-        if (s == nullptr) {
-            mAvatarAsset_Reward = avatarArray->Int(0);
+        DataNode &n = avatarArray->Node(1);
+        if (n.Type() == kDataInt) {
+            mAvatarAssetReward = n.Int();
         } else {
             MILO_NOTIFY(
-                "Invalid avatarasset_reward for %s (data type 0x%x)",
-                mAccomplishment_Name,
-                s
+                "Invalid avatarasset_reward for %s (data type 0x%x)", mName, n.Type()
             );
         }
     }
-
     static Symbol accomplishment_type("accomplishment_type");
-    i_pConfig->FindData(accomplishment_type, mAccomplishment_Type);
-
+    int type;
+    i_pConfig->FindData(accomplishment_type, type);
+    mAccomplishmentType = (AccomplishmentType)type;
     static Symbol category("category");
     i_pConfig->FindData(category, mCategory);
-
     static Symbol award("award");
     i_pConfig->FindData(award, mAward, false);
-
     static Symbol is_secondary_goal("is_secondary_goal");
-    i_pConfig->FindData(is_secondary_goal, isSecondaryGoal, false);
-
+    i_pConfig->FindData(is_secondary_goal, mIsSecondaryGoal, false);
     static Symbol units_token("units_token");
-    i_pConfig->FindData(units_token, mUnits_Token, false);
+    i_pConfig->FindData(units_token, mUnitsToken, false);
 }
--- src/lazer/meta_ham/Accomplishment.h
+++ src/lazer/meta_ham/Accomplishment.h
@@ -1,17 +1,49 @@
 #pragma once
-
 #include "hamobj/Difficulty.h"
+#include "meta_ham/HamProfile.h"
 #include "obj/Data.h"
 #include "ui/UILabel.h"
 #include "utl/Symbol.h"
+
+// stolen from RB3 lol
+enum AccomplishmentType {
+    kAccomplishmentTypeUnique = 0,
+    kAccomplishmentTypeSongListConditional = 1,
+    kAccomplishmentTypeSongFilterConditional = 2,
+    kAccomplishmentTypeLessonSongListConditional = 3,
+    kAccomplishmentTypeLessonDiscSongConditional = 4,
+    kAccomplishmentTypePlayerConditional = 5,
+    kAccomplishmentTypeTourConditional = 6,
+    kAccomplishmentTypeTrainerListConditional = 7,
+    kAccomplishmentTypeTrainerCategoryConditional = 8,
+    kAccomplishmentTypeOneShot = 9,
+    kAccomplishmentTypeSetlist = 10,
+    kAccomplishmentTypeDiscSongConditional = 11
+};
+
 class Accomplishment {
 public:
-    virtual ~Accomplishment();
-    virtual bool ShowBestAfterEarn() const;
-    virtual void UpdateIncrementalEntryName(UILabel *, Symbol);
-    virtual bool CanBeLaunched() const;
-
     Accomplishment(DataArray *, int);
+    virtual ~Accomplishment();
+    virtual AccomplishmentType GetType() const { return kAccomplishmentTypeUnique; }
+    virtual bool ShowBestAfterEarn() const;
+    virtual void UpdateIncrementalEntryName(UILabel *, Symbol) {
+        MILO_ASSERT(false, 0x4c);
+    }
+    virtual bool IsFulfilled(HamProfile *) const { return false; }
+    virtual bool IsRelevantForSong(Symbol) const { return false; }
+    virtual Difficulty GetRequiredDifficulty() const { return mDifficulty; }
+    virtual bool InqProgressValues(HamProfile *, int &, int &) { return false; }
+    virtual bool InqIncrementalSymbols(HamProfile *, std::vector<Symbol> &) const {
+        return false;
+    }
+    virtual bool IsSymbolEntryFulfilled(HamProfile *, Symbol) const { return false; }
+    virtual Symbol GetFirstUnfinishedAccomplishmentEntry(HamProfile *) const {
+        return gNullStr;
+    }
+    virtual bool CanBeLaunched() const;
+    virtual bool HasSpecificSongsToLaunch() const { return false; }
+
     Symbol GetCategory() const;
     bool HasGamerpicReward() const;
     bool HasAvatarAssetReward() const;
@@ -20,33 +52,39 @@ public:
     bool IsSecondaryGoal() const;
     bool IsDynamic() const;
     char const *GetIconArt() const;
-    Symbol GetName();
+    Symbol GetName() const;
     Difficulty GetRequiredDifficulty();
+    const std::vector<Symbol> &GetDynamicPrereqsSongs() const;
+    int GetDynamicPrereqsNumSongs() const;
+    int GetAvatarAssetReward() const;
+    int GetContextID() const;
+    int GetGamerpicReward() const;
 
-    Symbol mAccomplishment_Name; // 0x4
-    std::vector<Symbol> m_vSecret_Prereqs; // 0x8
-    int mAccomplishment_Type; // 0x14
+protected:
+    Symbol mName; // 0x4
+    std::vector<Symbol> mSecretPrereqs; // 0x8
+    AccomplishmentType mAccomplishmentType; // 0x14
     Symbol mCategory; // 0x18
     Symbol mAward; // 0x1c
-    Symbol mUnits_Token; // 0x20
-    int mDifficulty; // 0x24
-    Symbol mPassive_Msg_Channel; // 0x28
-    int mPassive_Msg_Priority; // 0x2c
-    bool mRequires_Unison_Ability; // 0x30
-    int mPlayerCount_Min; // 0x34
-    int mPlayerCount_Max; // 0x38
+    Symbol mUnitsToken; // 0x20
+    Difficulty mDifficulty; // 0x24
+    Symbol mPassiveMsgChannel; // 0x28
+    int mPassiveMsgPriority; // 0x2c
+    bool mRequiresUnisonAbility; // 0x30
+    int mPlayerCountMin; // 0x34
+    int mPlayerCountMax; // 0x38
     int mNumSongs; // 0x3c
-    std::vector<Symbol> m_vDynamic_Prereq_Songs; // 0x40
+    std::vector<Symbol> mDynamicPrereqsSongs; // 0x40
     int mProgressStep; // 0x4c
-    int mGamerpic_Reward; // 0x50
-    int mAvatarAsset_Reward; // 0x54
+    int mGamerpicReward; // 0x50
+    int mAvatarAssetReward; // 0x54
     bool mShowBestAfterEarn; // 0x58
     bool mHideProgress; // 0x59
-    int unk5c;
-    int mLastID; // 0x60
+    int mIndex; // 0x5c
+    int mContextID; // 0x60
     bool mEarnedNoFail; // 0x64
     bool mLeaderboard; // 0x65
-    bool isSecondaryGoal; // 0x66
+    bool mIsSecondaryGoal; // 0x66
     bool mGiveToAll; // 0x67
 
 private:
